3622418a069ae5c7c6c8feb532e9eb487f39ea23
Dipesh Jadhav <30771683+ProgrammingWithDJ@users.noreply.github.com>
2023-03-11 13:33:59 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+     strings: ["Graph API", "ASP.Net Web API", "Azure Development", "Angular"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
